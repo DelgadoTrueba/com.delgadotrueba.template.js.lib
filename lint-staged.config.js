c378e17affc437d59c74804ae02d7224b1c14f8b
Delgado Trueba <jose.delgado@babel.es>
2025-09-21 08:59:37 +0200

    refactor: utils folders

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 module.exports = {
-  'src/lib/*.ts': ['tsc --noEmit', 'eslint -c .eslintrc.json', 'npm run test -- --findRelatedTests'],
+  'src/utils-test/*.ts': ['tsc --noEmit', 'eslint -c .eslintrc.json'],
+  'src/**/*.ts': ['tsc --noEmit', 'eslint -c .eslintrc.json', 'npm run test -- --findRelatedTests'],
 };
