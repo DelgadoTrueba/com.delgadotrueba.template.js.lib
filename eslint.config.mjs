c0e4bf9669342f7ad3121f205f016630de10d9c2
Delgado Trueba <jose.delgado@babel.es>
2025-09-25 11:11:49 +0200

    feat: update prettier II

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import js from '@eslint/js';
-import ts from 'typescript-eslint';
-import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import prettierConfig from 'eslint-config-prettier';
+import pluginImport from "eslint-plugin-import";
+import globals from 'globals';
+import ts from 'typescript-eslint';
 
 export default defineConfig(
   globalIgnores([
@@ -26,15 +27,43 @@ export default defineConfig(
       },
     },
     {
-      files: ['*.{js,mjs,cjs}'],
+      files: ['**/*.{js}'],
       ...js.configs.recommended,
     },
     {
-      files: ['*.{ts,tsx,mts,cts}'],
+      files: ['**/*.{ts}'],
       ...ts.configs.recommended,
-    },
-    {
-      rules: {}
+      plugins: { import: pluginImport },
+      settings: {
+        // Para que "internal" respete tus paths de TS (paths/alias en tsconfig)
+        "import/resolver": {
+          typescript: { project: true }
+        }
+      },
+      rules: {
+        // 🔣 Reglas para los imports <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        "@typescript-eslint/consistent-type-imports": ["error", {
+          prefer: "type-imports",
+          fixStyle: "separate-type-imports"
+        }],
+        // pluginImport
+        "import/order": ["error", {
+          "groups": [
+            "builtin",        // node:fs, path
+            "external",       // react, lodash
+            "internal",       // alias de tu app (tsconfig paths)
+            ["parent", "sibling", "index"], // imports relativos sin saltos
+            "object",
+            "type"            // type-only imports
+          ],
+          // ¡Importante! Deja que tus pathGroups afecten también a "external"
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          // Espaciado y orden alfabético
+          "newlines-between": "always",
+          "alphabetize": { order: "asc", caseInsensitive: true }
+        }],
+        "import/no-duplicates": "error"
+      }
     },
     // Quita reglas de estilo que choquen con Prettier
     prettierConfig,
