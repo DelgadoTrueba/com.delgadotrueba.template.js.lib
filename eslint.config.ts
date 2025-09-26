fd1d93a012c855b2652dbe3b488ede4b80d2fdbc
Delgado Trueba <jose.delgado@babel.es>
2025-09-26 11:19:14 +0200

    feat: final eslint

---
+++ eslint.config.ts
@@ -0,0 +1,83 @@
+import { configs as jsConfigs } from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import { importX } from 'eslint-plugin-import-x';
+import globals from 'globals';
+import { configs as tsConfigs } from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default defineConfig([
+  globalIgnores([
+    'node_modules',
+    'dist',
+    'coverage',
+    'scripts',
+    'commitlint.config.js',
+    'jest.cjs.config.js',
+    'jest.mjs.config.js',
+    'jest.preset.js',
+    'lint-staged.config.js',
+  ]),
+  // Entorno Global (define las variable globales de node / browser)
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+
+  // Presets para JS
+  jsConfigs.recommended,
+  importX.flatConfigs.recommended,
+
+  // Presets para TS
+  ...tsConfigs.recommended,
+  importX.flatConfigs.typescript,
+
+  // Capa de reglas propia para TS
+  {
+    files: ['**/*.ts'],
+    settings: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
+          project: './tsconfig.base.json',
+        }),
+      ],
+    },
+    rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      // 🔣 Reglas para los imports <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+      // pluginImportX
+      'import-x/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // node:fs, path
+            'external', // react, lodash
+            'internal', // alias de tu app (tsconfig paths)
+            ['parent', 'sibling', 'index'], // imports relativos sin saltos
+            'object',
+            'type', // type-only imports
+          ],
+          // ¡Importante! Deja que tus pathGroups afecten también a "external"
+          pathGroupsExcludedImportTypes: ['builtin'],
+          // Espaciado y orden alfabético
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import-x/no-duplicates': 'error',
+    },
+  },
+  // 🔕 Apaga reglas de estilo conflictivas con Prettier
+  prettierConfig,
+]);
